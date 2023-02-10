207b3f4a4cb39077be61a748c6a62db7f5043144
qwertyuiop098ghdjksjhkdsjn <anastasia.spasova@mail.ru>
2023-02-10 14:06:34 +0300

    second commit

--- task1.js
+++ task1.js
@@ -1,24 +1,27 @@
-let arr = [0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, null];
-let countEven = 0; 
-let countOdd = 0; 
-let countOther = 0; 
-let countZero = 0; 
-for (let i = 0; i < arr.length; i++){
-    if (isNaN(arr[i]) || (typeof(arr[i]) !== 'number')) {
-        countOther++;
-    } else {
-        if (arr[i]===0) {
-					countZero++
-				}
-				else if (arr[i] % 2 === 0) {
-            countEven++;
-        } 
-      	else {
-            countOdd++;
+function number(){
+    let arr = [0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, null];
+    let countEven = 0; //чётное
+    let countOdd = 0;  //нечётное
+    let countOther = 0; //не число
+    let countZero = 0; //ноль
+    for (let i = 0; i < arr.length; i++){
+        if (isNaN(arr[i]) || (typeof(arr[i]) !== 'number')) {
+            countOther++;
+        } else {
+            if (arr[i]===0) {
+                        countZero++
+                    }
+                    else if (arr[i] % 2 === 0) {
+                countEven++;
+            } 
+              else {
+                countOdd++;
+            }
         }
     }
-}
-console.log("чётных чисел " + countEven);
-console.log("нечётных чисел " + countOdd);
-console.log("не число " + countOther);
-console.log("ноль элементов " + countZero)
+    console.log("чётных чисел " + countEven);
+    console.log("нечётных чисел " + countOdd);
+    console.log("не число " + countOther);
+    console.log("ноль элементов " + countZero)
+    }
+    number();
